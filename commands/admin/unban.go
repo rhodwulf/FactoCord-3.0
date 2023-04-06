eaae2070a54534e8a2b827ac8a63ce14dcd19625
Rhodwulf <rhodwulford@gmail.com>
2023-04-06 13:51:05 -0600

    Traducción unban.go
    Traducción al español.

--- commands/admin/unban.go
+++ commands/admin/unban.go
@@ -11,20 +11,20 @@ import (
 var UnbanPlayerDoc = support.CommandDoc{
 	Name:  "unban",
 	Usage: "$unban <player>",
-	Doc:   `command removes the player from the banlist on the server`,
+	Doc:   `El comando elimina al jugador de la lista de prohibición en el servidor.`,
 }
 
 // UnbanPlayer unbans a player on the server.
 func UnbanPlayer(s *discordgo.Session, args string) {
 	if strings.ContainsAny(args, " \n\t") {
-		support.SendFormat(s, "Usage: "+UnbanPlayerDoc.Usage)
+		support.SendFormat(s, "Uso: "+UnbanPlayerDoc.Usage)
 		return
 	}
 	command := "/unban " + args
 	success := support.Factorio.Send(command)
 	if success {
-		support.Send(s, "Player "+args+" unbanned!")
+		support.Send(s, "Jugador "+args+" ha sido desbaneado!")
 	} else {
-		support.Send(s, "Sorry, there was an error sending /unban command")
+		support.Send(s, "Lo sentimos, hubo un error al enviar el comando /unban")
 	}
 }
